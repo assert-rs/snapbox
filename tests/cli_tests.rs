37f11886d770c9023c390c4d4bc554241ce48365
Ed Page <eopage@gmail.com>
2021-11-05 16:48:04 -0500

    test: Address windows

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -1,6 +1,8 @@
 #[test]
 fn cli_tests() {
-    trycmd::TestCases::new()
-        .case("tests/cmd/*.trycmd")
-        .case("tests/cmd/*.toml");
+    let t = trycmd::TestCases::new();
+    t.case("tests/cmd/*.trycmd").case("tests/cmd/*.toml");
+    // On windows, crashes are returned as code=1
+    #[cfg(target_os = "windows")]
+    t.skip("tests/cmd/timeout.toml");
 }
