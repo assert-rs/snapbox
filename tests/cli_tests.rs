5f9dacfc066b4aac61140f791f6fac3fa1369b0b
Ed Page <eopage@gmail.com>
2021-11-06 08:23:20 -0500

    refactor: Generate schema as part of tests

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -2,6 +2,8 @@
 fn cli_tests() {
     let t = trycmd::TestCases::new();
     t.case("tests/cmd/*.trycmd").case("tests/cmd/*.toml");
+    #[cfg(not(feature = "schema"))]
+    t.skip("tests/cmd/schema.trycmd");
     // On windows, crashes are returned as code=1
     #[cfg(target_os = "windows")]
     t.skip("tests/cmd/timeout.toml");
