1b02d5d2b0954a032c4f93db64a903fda97f9e5d
Ed Page <eopage@gmail.com>
2021-11-08 16:09:28 -0600

    refactor: Reduce cfg's

--- src/cargo.rs
+++ src/cargo.rs
@@ -22,74 +22,79 @@ macro_rules! cargo_bin {
     };
 }
 
-/// Prepare an example for testing
-///
-/// Unlike [`trycmd::cargo_bin!`], this does not inherit all of the current compiler settings.  It
-/// will match the current target and profile but will not get feature flags.  Pass those arguments
-/// to the compiler via `args`.
-///
-/// ## Example
-///
-/// ```rust,no_run
-/// #[test]
-/// fn cli_tests() {
-///     trycmd::TestCases::new()
-///         .register_bin("example-fixture", trycmd::cargo::compile_example("example-fixture", []))
-///         .case("examples/cmd/*.trycmd");
-/// }
-/// ```
 #[cfg(feature = "examples")]
-pub fn compile_example<'a>(
-    name: &str,
-    args: impl IntoIterator<Item = &'a str>,
-) -> Result<std::path::PathBuf, crate::Error> {
-    eprintln!("Compiling example {}", name);
-    let messages = escargot::CargoBuild::new()
-        .current_target()
-        .current_release()
-        .example(name)
-        .args(args)
-        .exec()
-        .map_err(|e| crate::Error::new(e.to_string()))?;
-    for message in messages {
-        let message = message.map_err(|e| crate::Error::new(e.to_string()))?;
-        let message = message
-            .decode()
-            .map_err(|e| crate::Error::new(e.to_string()))?;
-        eprintln!("Message: {:?}", message);
-        match message {
-            escargot::format::Message::CompilerMessage(msg) => {
-                let level = msg.message.level;
-                if level == escargot::format::diagnostic::DiagnosticLevel::Ice
-                    || level == escargot::format::diagnostic::DiagnosticLevel::Error
-                {
-                    return Err(crate::Error::new(
-                        msg.message
-                            .rendered
-                            .unwrap_or(msg.message.message)
-                            .into_owned(),
-                    ));
-                }
-            }
-            escargot::format::Message::CompilerArtifact(artifact) => {
-                if !artifact.profile.test
-                    && artifact.target.crate_types == ["bin"]
-                    && artifact.target.kind == ["example"]
-                {
-                    let path = artifact
-                        .executable
-                        .expect("cargo is new enough for this to be present");
-                    return Ok(path.into_owned());
-                }
-            }
-            _ => {}
-        }
-    }
+pub use examples::compile_example;
 
-    Err(crate::Error::new(format!(
-        "Unknown error building example {}",
-        name
-    )))
+#[cfg(feature = "examples")]
+pub(crate) mod examples {
+    /// Prepare an example for testing
+    ///
+    /// Unlike [`trycmd::cargo_bin!`], this does not inherit all of the current compiler settings.  It
+    /// will match the current target and profile but will not get feature flags.  Pass those arguments
+    /// to the compiler via `args`.
+    ///
+    /// ## Example
+    ///
+    /// ```rust,no_run
+    /// #[test]
+    /// fn cli_tests() {
+    ///     trycmd::TestCases::new()
+    ///         .register_bin("example-fixture", trycmd::cargo::compile_example("example-fixture", []))
+    ///         .case("examples/cmd/*.trycmd");
+    /// }
+    /// ```
+    pub fn compile_example<'a>(
+        name: &str,
+        args: impl IntoIterator<Item = &'a str>,
+    ) -> Result<std::path::PathBuf, crate::Error> {
+        eprintln!("Compiling example {}", name);
+        let messages = escargot::CargoBuild::new()
+            .current_target()
+            .current_release()
+            .example(name)
+            .args(args)
+            .exec()
+            .map_err(|e| crate::Error::new(e.to_string()))?;
+        for message in messages {
+            let message = message.map_err(|e| crate::Error::new(e.to_string()))?;
+            let message = message
+                .decode()
+                .map_err(|e| crate::Error::new(e.to_string()))?;
+            eprintln!("Message: {:?}", message);
+            match message {
+                escargot::format::Message::CompilerMessage(msg) => {
+                    let level = msg.message.level;
+                    if level == escargot::format::diagnostic::DiagnosticLevel::Ice
+                        || level == escargot::format::diagnostic::DiagnosticLevel::Error
+                    {
+                        return Err(crate::Error::new(
+                            msg.message
+                                .rendered
+                                .unwrap_or(msg.message.message)
+                                .into_owned(),
+                        ));
+                    }
+                }
+                escargot::format::Message::CompilerArtifact(artifact) => {
+                    if !artifact.profile.test
+                        && artifact.target.crate_types == ["bin"]
+                        && artifact.target.kind == ["example"]
+                    {
+                        let path = artifact
+                            .executable
+                            .expect("cargo is new enough for this to be present");
+                        return Ok(path.into_owned());
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        Err(crate::Error::new(format!(
+            "Unknown error building example {}",
+            name
+        )))
+    }
 }
 
 /// Look up the path to a cargo-built binary within an integration test.
