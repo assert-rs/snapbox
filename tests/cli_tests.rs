e4365dbf67b248629e00fc4bfdaa18b6c71270a9
Ed Page <eopage@gmail.com>
2021-11-10 13:31:12 -0600

    feat: Multi-step `.trycmd` files
    This allows sequencing commands or just testing multiple case in a
    single file.

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -3,12 +3,18 @@ fn cli_tests() {
     let t = trycmd::TestCases::new();
     t.case("tests/cmd/*.trycmd").case("tests/cmd/*.toml");
     #[cfg(not(feature = "schema"))]
-    t.skip("tests/cmd/schema.toml");
+    {
+        t.skip("tests/cmd/schema.toml");
+    }
     #[cfg(not(feature = "filesystem"))]
-    t.skip("tests/cmd/sandbox.toml");
-    #[cfg(not(feature = "filesystem"))]
-    t.skip("tests/cmd/normalize.toml");
-    // On windows, crashes are returned as code=1
+    {
+        t.skip("tests/cmd/sandbox.toml");
+        t.skip("tests/cmd/multistep.trycmd");
+        t.skip("tests/cmd/normalize.toml");
+    }
     #[cfg(target_os = "windows")]
-    t.skip("tests/cmd/timeout.toml");
+    {
+        // On windows, crashes are returned as code=1
+        t.skip("tests/cmd/timeout.toml");
+    }
 }
