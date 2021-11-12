6e611ac8b0d7a0fc8e27ff136301768ce193d0c3
Ed Page <eopage@gmail.com>
2021-11-12 10:43:30 -0600

    feat: Add backtraces to errors
    Fixes #35

--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,36 @@
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug)]
 pub struct Error {
     inner: String,
+    backtrace: Option<Backtrace>,
 }
 
 impl Error {
     #[allow(dead_code)]
     pub(crate) fn new(inner: String) -> Self {
-        Self { inner }
+        Self {
+            inner,
+            backtrace: Backtrace::new(),
+        }
     }
 }
 
+impl PartialEq for Error {
+    fn eq(&self, other: &Self) -> bool {
+        self.inner == other.inner
+    }
+}
+
+impl Eq for Error {}
+
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.inner.fmt(f)
+        writeln!(f, "{}", self.inner)?;
+        if let Some(backtrace) = self.backtrace.as_ref() {
+            writeln!(f)?;
+            writeln!(f, "Backtrace:")?;
+            writeln!(f, "{}", backtrace)?;
+        }
+        Ok(())
     }
 }
 
@@ -33,5 +51,42 @@ impl<'s> From<&'s String> for Error {
 impl From<String> for Error {
     fn from(other: String) -> Self {
         Self::new(other)
+    }
+}
+
+#[cfg(feature = "debug")]
+#[derive(Debug, Clone)]
+struct Backtrace(backtrace::Backtrace);
+
+#[cfg(feature = "debug")]
+impl Backtrace {
+    fn new() -> Option<Self> {
+        Some(Self(backtrace::Backtrace::new()))
+    }
+}
+
+#[cfg(feature = "debug")]
+impl std::fmt::Display for Backtrace {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        // `backtrace::Backtrace` uses `Debug` instead of `Display`
+        write!(f, "{:?}", self.0)
+    }
+}
+
+#[cfg(not(feature = "debug"))]
+#[derive(Debug, Copy, Clone)]
+struct Backtrace;
+
+#[cfg(not(feature = "debug"))]
+impl Backtrace {
+    fn new() -> Option<Self> {
+        None
+    }
+}
+
+#[cfg(not(feature = "debug"))]
+impl std::fmt::Display for Backtrace {
+    fn fmt(&self, _: &mut std::fmt::Formatter) -> std::fmt::Result {
+        Ok(())
     }
 }
