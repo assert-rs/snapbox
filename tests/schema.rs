c4ff0968b0a1d283bbcb9959e4126ea57ac5adf2
Ed Page <eopage@gmail.com>
2022-05-06 10:31:35 -0500

    refactor: Fill in linked schema
    The docs link to this but github doesn't handle the symlink well.

---
+++ tests/schema.rs
@@ -0,0 +1,9 @@
+#[test]
+#[cfg(feature = "schema")]
+fn dump_schema() {
+    let bin_path = snapbox::cmd::cargo_bin!("trycmd-schema");
+    snapbox::cmd::Command::new(bin_path)
+        .assert()
+        .success()
+        .stdout_eq_path("schema.json");
+}
