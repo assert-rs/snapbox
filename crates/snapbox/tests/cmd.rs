493974d977effc5a680ca62c6aa7bd874a1bb86b
Ed Page <eopage@gmail.com>
2022-08-15 15:30:51 -0500

    test(snap): Basic output tests

---
+++ crates/snapbox/tests/cmd.rs
@@ -0,0 +1,17 @@
+#[test]
+fn regular_stdout_split() {
+    snapbox::cmd::Command::new(snapbox::cmd::cargo_bin("snap-fixture"))
+        .env("echo_cwd", "1")
+        .assert()
+        .success();
+}
+
+#[test]
+#[cfg(feature = "cmd")]
+fn regular_stdout_single() {
+    snapbox::cmd::Command::new(snapbox::cmd::cargo_bin("snap-fixture"))
+        .env("echo_cwd", "1")
+        .stderr_to_stdout()
+        .assert()
+        .success();
+}
