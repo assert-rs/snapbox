b5949724922ceaf395cc29161db8f124ee608e1a
Ed Page <eopage@gmail.com>
2024-02-14 09:14:31 -0600

    feat(snap): Expose cargo_rustc_current_dir

--- crates/snapbox/src/macros.rs
+++ crates/snapbox/src/macros.rs
@@ -3,7 +3,19 @@
 #[macro_export]
 macro_rules! current_dir {
     () => {{
-        let root = if let Some(rustc_root) = ::std::option_env!("CARGO_RUSTC_CURRENT_DIR") {
+        let root = $crate::path::cargo_rustc_current_dir!();
+        let file = ::std::file!();
+        let rel_path = ::std::path::Path::new(file).parent().unwrap();
+        root.join(rel_path)
+    }};
+}
+
+/// Find the base directory for [`std::file!`]
+#[doc(hidden)] // forced to be visible in intended location
+#[macro_export]
+macro_rules! cargo_rustc_current_dir {
+    () => {{
+        if let Some(rustc_root) = ::std::option_env!("CARGO_RUSTC_CURRENT_DIR") {
             ::std::path::Path::new(rustc_root)
         } else {
             let manifest_dir = ::std::path::Path::new(::std::env!("CARGO_MANIFEST_DIR"));
@@ -12,9 +24,6 @@ macro_rules! current_dir {
                 .filter(|it| it.join("Cargo.toml").exists())
                 .last()
                 .unwrap()
-        };
-        let file = ::std::file!();
-        let rel_path = ::std::path::Path::new(file).parent().unwrap();
-        root.join(rel_path)
+        }
     }};
 }
