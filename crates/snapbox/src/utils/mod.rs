b2b1b3034c345a3ac158064079c5f7a756af9483
Ed Page <eopage@gmail.com>
2024-05-15 16:03:54 -0500

    fix: Deprecate normalize_text

--- crates/snapbox/src/utils/mod.rs
+++ crates/snapbox/src/utils/mod.rs
@@ -24,6 +24,10 @@ pub fn normalize_paths(data: &str) -> String {
 /// This includes
 /// - Line endings
 /// - Path separators
+#[deprecated(
+    since = "0.5.11",
+    note = "Replaced with `filter::normalize_paths(filter::normalize_lines(...))`"
+)]
 pub fn normalize_text(data: &str) -> String {
     #[allow(deprecated)]
     normalize_paths(&normalize_lines(data))
