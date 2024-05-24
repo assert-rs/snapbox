b288ef50c561dee695ad90fad435ff3ffeff6f07
Ed Page <eopage@gmail.com>
2024-05-24 15:20:43 -0500

    fix(data): Show relative path for inline str

--- crates/snapbox/src/data/source.rs
+++ crates/snapbox/src/data/source.rs
@@ -109,6 +109,12 @@ pub struct Position {
 
 impl std::fmt::Display for Position {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}:{}:{}", self.file.display(), self.line, self.column)
+        write!(
+            f,
+            "{}:{}:{}",
+            crate::dir::display_relpath(&self.file),
+            self.line,
+            self.column
+        )
     }
 }
