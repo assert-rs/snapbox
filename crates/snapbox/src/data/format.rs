9cecb4a2c166759219dfa34e011f6fbc3b771007
Ed Page <eopage@gmail.com>
2025-10-15 15:22:17 -0500

    test(snapbox): Verify inferring formats

--- crates/snapbox/src/data/format.rs
+++ crates/snapbox/src/data/format.rs
@@ -57,3 +57,44 @@ impl From<&std::path::Path> for DataFormat {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn combos() {
+        #[cfg(feature = "json")]
+        let json = DataFormat::Json;
+        #[cfg(not(feature = "json"))]
+        let json = DataFormat::Text;
+        #[cfg(feature = "json")]
+        let jsonl = DataFormat::JsonLines;
+        #[cfg(not(feature = "json"))]
+        let jsonl = DataFormat::Text;
+        #[cfg(feature = "term-svg")]
+        let term_svg = DataFormat::TermSvg;
+        #[cfg(not(feature = "term-svg"))]
+        let term_svg = DataFormat::Text;
+        let cases = [
+            ("foo", DataFormat::Text),
+            (".foo", DataFormat::Text),
+            ("foo.txt", DataFormat::Text),
+            (".foo.txt", DataFormat::Text),
+            ("foo.stdout.txt", DataFormat::Text),
+            ("foo.json", json),
+            ("foo.stdout.json", DataFormat::Text),
+            (".foo.json", DataFormat::Text),
+            ("foo.jsonl", jsonl),
+            ("foo.stdout.jsonl", DataFormat::Text),
+            (".foo.jsonl", DataFormat::Text),
+            ("foo.term.svg", term_svg),
+            ("foo.stdout.term.svg", DataFormat::Text),
+            (".foo.term.svg", DataFormat::Text),
+        ];
+        for (input, output) in cases {
+            let input = std::path::Path::new(input);
+            assert_eq!(DataFormat::from(input), output, "for `{}`", input.display());
+        }
+    }
+}
